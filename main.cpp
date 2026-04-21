342b4c7c018cd3d9ce0dfdd0a983d7e7ccabaed1
huang186 <2150578464@qq.com>
2026-04-22 00:16:04 +0800

    3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,69 @@
 #include<iostream>
 #include<string>
+#include<fstream>
+#include<map>
 int main(){
     std::cout<<"欢迎进入linux系统中文辅助器\n";
     std::cout<<"\\";
-    system("cd");
+    std::system("cd");
+    std::string input;
+    std::string temp;
+    std::map<std::string,std::string>cmd_map;
+    std::ifstream file("config");
+    if(!file.is_open()){
+        std::cout<<"配置文件不存在";
+    }
+    while(std::getline(file,temp)){
+        if(temp.empty()){
+            file.close();
+            std::cout<<"配置文件为空,请使用set命令添加指令\n";
+            break;
+        }
+        int add=0;
+        for(int i=0;i<temp.length();i++){
+            if(temp[i]==' '){
+                add=i;
+                break;
+            }
+        }
+        std::string string_temp1=temp.substr(0,add);
+        std::string string_temp2=temp.substr(add+1,temp.length()-1);
+        cmd_map[string_temp1]=string_temp2;
+
+    }
+    std::string add="\\";
+    while(1){
+        std::getline(std::cin,input);
+        if(input=="exit"){
+            std::cout<<"感谢使用linux系统中文辅助器\n";
+            return 0;
+        }
+        if(input=="set"){
+            std::ofstream file("config",std::ios::app);
+            file<<"\n";
+            std::cout<<"请输入新增命令:";
+            std::string cmd;
+            std::getline(std::cin,cmd);
+            file<<cmd;
+            file<<" ";
+            std::cout<<"请输入命令对应的linux指令:";
+            std::getline(std::cin,cmd);
+            file<<cmd;
+            file.close();
+        }
+        std::string str;
+        size_t space_pos=input.find(' ');
+        std::string base_command=input.substr(0, space_pos);
+        std::string extra_args=input.substr(space_pos);
+        auto it=cmd_map.find(base_command);
+        if(it!=cmd_map.end()){
+            str=cmd_map[base_command]+extra_args;
+            std::system(str.c_str());
+        }
+        else{
+            std::system(input.c_str());
+        }
+        std::cout<<"\\";
+    }
+    return 0;
 }
